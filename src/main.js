2b2e2dded386ebc262397e5b28374678685a7026
griFFin <134530165+griffin3q@users.noreply.github.com>
2024-10-11 13:26:25 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
 var audio = document.getElementById("bgMusic");
 audio.volume = 0.01;
+
+var flip = document.getElementById("flip-card");
+flip.addEventListener("click",function flipIt(){
+    flip.classList.add("flip")
+    flip.src=""
+})
