a81366bf165509bd3ae96b0e911f6ade676361ea
griFFin <134530165+griffin3q@users.noreply.github.com>
2024-10-13 21:37:20 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 var audio = document.getElementById("bgMusic");
 audio.volume = 0.01;
 
+var love = document.getElementById("audio-img");
+var playerStatus = 0;
+var oldSrc = "src/bgMusic.mp3"
+
+love.addEventListener("click", function click() {
+    console.log("Kocham moją Mądrą Dziewczynke <3")
+    if (playerStatus == 0) {
+        audio.src = ""
+        playerStatus = 1
+        love.style.animationName = "none"
+    }
+    else {
+        audio.src = oldSrc
+        playerStatus = 0
+        love.style.animationName = "spin"
+    }
+})
+
+
 var flip = document.getElementById("flip-card");
-flip.addEventListener("click",function flipIt(){
+flip.addEventListener("click", function flipIt() {
     flip.classList.add("flip")
-    flip.src=""
+    flip.src = ""
 })
